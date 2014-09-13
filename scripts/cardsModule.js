a540810cb3e25d93f0af46814d7550e9cd3498df
Toshiyuki Koyama <koyto01@yahoo.co.jp>
2014-09-13 13:47:43 +0000

    added card images.

--- scripts/cardsModule.js
+++ scripts/cardsModule.js
@@ -1,42 +1,50 @@
 angular.module('cardsModule', [])
-    .value('CARD_PICS', ["card1", "card2", "card3", "card4", "card5", "card6",
-                        "card7", "card8", "card9", "card10", "card11", "card12"])
+    .value('CARD_PICS', 
+    ["images/c01.gif", "images/c02.gif", "images/c03.gif", "images/c04.gif", "images/c05.gif", "images/c06.gif", "images/c07.gif",
+    "images/c08.gif",  "images/c09.gif", "images/c10.gif", "images/c11.gif", "images/c12.gif", "images/c13.gif", "images/d01.gif",
+    "images/d02.gif",  "images/d03.gif", "images/d04.gif", "images/d05.gif", "images/d06.gif", "images/d07.gif", "images/d08.gif",
+    "images/d09.gif",  "images/d10.gif", "images/d11.gif", "images/d12.gif", "images/d13.gif", "images/h01.gif", "images/h02.gif",
+    "images/h03.gif",  "images/h04.gif", "images/h05.gif", "images/h06.gif", "images/h07.gif", "images/h08.gif", "images/h09.gif",
+    "images/h10.gif",  "images/h11.gif", "images/h12.gif", "images/h13.gif", "images/s01.gif", "images/s02.gif", "images/s03.gif",
+    "images/s04.gif",  "images/s05.gif", "images/s06.gif", "images/s07.gif", "images/s08.gif", "images/s09.gif", "images/s10.gif",
+    "images/s11.gif",  "images/s12.gif", "images/s13.gif"])
                         
-    .value('CARD_BACK_PIC', "backsize")
+    .value('CARD_BACK_PIC', "images/z02.gif")
 
     .factory('cardsConfig', function (CARD_PICS) {
-        this.numCols = 3;
+        this.numCols = 13;
         this.numRaws = CARD_PICS.length / this.numCols;
         return this;
     })
-
+    
     .factory('cards', function (CARD_PICS, CARD_BACK_PIC, cardsConfig) {
         var cardsArray = [],
             raw,
             column,
             idx = 0,
+            
             CardObj = function (rawnum, colnum, picValue, picId, isForeside) {
-                return {
-                    raw: rawnum,
-                    col: colnum,
-                    pic: picValue,
-                    picId: picId,
-                    foreside: isForeside,
-                    toggleSide: function () {
-                        this.foreside = !this.foreside;
-                    },
-                    resource: function () {
-                        if (this.foreside) {
-                            return this.pic;
-                        }
-                        return CARD_BACK_PIC;
-                    },
-                    class: function () {
-                        if (this.foreside) {
-                            return "card-front";
-                        }
-                        return "card-back";
+                this.raw = rawnum;
+                this.col = colnum;
+                this.pic = picValue;
+                this.picId = picId;
+                this.foreside = isForeside;
+                this.toggleSide = function () {
+                    this.foreside = !this.foreside;
+                };
+                
+                this.resource = function () {
+                    if (this.foreside) {
+                        return this.pic;
                     }
+                    return CARD_BACK_PIC;
+                };
+                
+                this.class = function () {
+                    if (this.foreside) {
+                        return "card-front";
+                    }
+                    return "card-back";
                 };
             };
 
